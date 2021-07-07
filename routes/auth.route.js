4d44b264807475c0d75b462a4be3d33892f8d48c
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-07-06 17:11:11 -0700

    refactor: DELETE request for logout instead of GET

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,6 +6,6 @@ router.route("/login").post(authController.login);
 
 router.route("/signup").post(authController.signup);
 
-router.get("/logout", authController.logout);
+router.delete("/logout", authController.logout);
 
 module.exports = router;
