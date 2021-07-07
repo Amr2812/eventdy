3a19548a931dfbe1e30149e66172985d70001bac
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-07-06 23:00:44 -0700

    fix: mongodb useFindAndModify

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
   })
   .then(console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
